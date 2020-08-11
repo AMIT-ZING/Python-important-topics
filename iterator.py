9e726e5d2d20c0ae1472a7a79facbb1e68f0a4b4
AMIT KUMAR <amit49376@gmail.com>
2020-08-11 20:27:11 +0530

    ITERATOR

---
+++ iterator.py
@@ -0,0 +1,10 @@
+string = [1, 2, 4, 6, 7, 8]
+
+i_string = string.__iter__()
+
+print(next(i_string))
+print(next(i_string))
+print(next(i_string))
+print(next(i_string))
+print(next(i_string))
+print(next(i_string))
